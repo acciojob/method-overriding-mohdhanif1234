bc5b61e1ef12243ae828f28ba7bebd81efd75cbb
mohdhanif1234 <hanif.topia@gmail.com>
2022-12-10 14:24:03 +0530

    Method Overriding In Java assignment submitted

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+    public static class A{ // creating class A
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{ // creating class B and inheriting it from class A
+        public String meth(){
+            return "Method is overridden in Extended class B";
+        }
+    }
+  public static void main(String[] args){
+        B objB = new B(); // creating object of class B
+        System.out.println(objB.meth());
+  }
 }
